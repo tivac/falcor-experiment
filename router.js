eb3a20070ec291aded930523f67441df210c2929
Pat Cavit <pcavit@gmail.com>
2015-08-19 00:01:48 -0700

    Router getting GW2 item details! :confetti_ball:

--- router.js
+++ router.js
@@ -1,17 +1,85 @@
 "use strict";
 
+var axios = require("axios");
+
 module.exports = require("falcor-router").createClass([ {
-    // match a request for the key "greeting"    
-    route : "greeting",
-    
-    // respond with a PathValue with the value of "Hello World."
-    get : function() {
-        return {
-            path : [
-                "greeting"
-            ],
-            
-            value : "Hello World"
-        };
+    route : "items[{integers:ids}]['name', 'type', 'level', 'rarity', 'icon']",
+    get   : function(pathSet) {
+        return axios.get("https://api.guildwars2.com/v2/items?ids=" + pathSet.ids.join(","))
+            .then(function(resp) {
+                var map     = {},
+                    results = [];
+                
+                resp.data.forEach(function(item) {
+                    map[item.id] = item;
+                });
+                
+                pathSet.ids.forEach(function(id) {
+                    pathSet[2].forEach(function(key) {
+                        if(!map[id]) {
+                            return results.push({
+                                path  : [ "items", id, key ],
+                                value : {
+                                    "$type" : "error",
+                                    value   : "Unknown item"
+                                }
+                            });
+                        }
+                        
+                        if(!map[id][key]) {
+                            return results.push({
+                                path  : [ "items", id, key ],
+                                value : {
+                                    "$type" : "error",
+                                    value   : "Unknown field"
+                                }
+                            });
+                        }
+                        
+                        results.push({
+                            path  : [ "items", id, key ],
+                            value : map[id][key]
+                        });
+                    });
+                });
+                
+                return results;
+            });
+    }
+}, {
+    route : "items[{integers:ids}]['buys', 'sells']['price', 'quantity']",
+    get   : function(pathSet) {
+        return axios.get("https://api.guildwars2.com/v2/commerce/prices?ids=" + pathSet.ids.join(","))
+            .then(function(resp) {
+                var map     = {},
+                    results = [];
+                
+                resp.data.forEach(function(item) {
+                    map[item.id] = item;
+                });
+                
+                pathSet.ids.forEach(function(id) {
+                    pathSet[2].forEach(function(type) {
+                        pathSet[3].forEach(function(field) {
+                            if(!map[id]) {
+                                return results.push({
+                                    path  : [ "items", id, type, field ],
+                                    value : {
+                                        "$type" : "error",
+                                        value   : "Unknown item"
+                                    }
+                                });
+                            }
+                            
+                            results.push({
+                                path  : [ "items", id, type, field ],
+                                value : map[id][type][field === "price" ? "unit_price" : field]
+                            });
+                        });
+                    });
+                });
+                
+                return results;
+            });
     }
 } ]);
