11390a96c8d014b6e178090412fe7db852340168
Pat Cavit <pcavit@gmail.com>
2015-08-20 22:50:09 -0700

    Fixing indentation (& my range math)

--- router.js
+++ router.js
@@ -19,25 +19,36 @@ module.exports = require("falcor-router").createClass([ {
         // Using axios all because we may have been passed multiple ranges
         return axios.all(
             pathSet.ids.map(function(range) {
-                var size = Math.max(1, range.to - range.from),
-                    page = Math.floor(range.from / size);
+                // Adding one to size because ranges are inclusive
+                var size  = (range.to + 1) - range.from,
+                    page  = Math.floor(range.from / size),
+                    // Determine starting index so we can filter out results
+                    start = size * page,
+                    // Sometimes page starts won't line up nicely
+                    diff  = range.from - start;
                 
-                return axios.get("https://api.guildwars2.com/v2/items?page=" + page + "&page_size=" + size)
-                    .then(function(resp) {
-                        return resp.data
-                            .sort(function(a, b) {
-                                return a.id - b.id;
-                            })
-                            .map(function(item, idx) {
-                                return {
-                                    path  : [ "items", range.from + idx ],
-                                    value : {
-                                        $type : "ref",
-                                        value : [ "itemsById", item.id ]
-                                    }
-                                };
-                            });
-                    });
+                console.log("https://api.guildwars2.com/v2/items?page=" + page + "&page_size=" + (size + diff));
+                
+                return axios.get(
+                    "https://api.guildwars2.com/v2/items?page=" + page + "&page_size=" + (size + diff)
+                ).then(function(resp) {
+                    return resp.data
+                        .sort(function(a, b) {
+                            return a.id - b.id;
+                        })
+                        .filter(function(item, idx) {
+                            return start + idx >= range.from;
+                        })
+                        .map(function(item, idx) {
+                            return {
+                                path  : [ "items", range.from + idx ],
+                                value : {
+                                    $type : "ref",
+                                    value : [ "itemsById", item.id ]
+                                }
+                            };
+                        });
+                });
             })
         )
         .then(function(ranges) {
@@ -49,25 +60,75 @@ module.exports = require("falcor-router").createClass([ {
             
             return paths;
         });
-        
     }
 }, {
-    route : "itemsById[{integers:ids}]['id', 'name', 'type', 'level', 'rarity', 'icon']",
+    route : "itemsById[{integers:ids}].details[{keys:fields}]",
     get   : function(pathSet) {
-        return axios.get("https://api.guildwars2.com/v2/items?ids=" + pathSet.ids.join(","))
-            .then(function(resp) {
-                var map     = {},
-                    results = [];
+        return axios.get(
+            "https://api.guildwars2.com/v2/items?ids=" + pathSet.ids.join(",")
+        )
+        .then(function(resp) {
+            var map     = {},
+                results = [];
+            
+            resp.data.forEach(function(item) {
+                map[item.id] = item;
+            });
+            
+            pathSet.ids.forEach(function(id) {
+                if(!map[id]) {
+                    return results.push({
+                        path  : [ pathSet[0], id ],
+                        value : {
+                            $type : "error",
+                            value : "Unknown item"
+                        }
+                    });
+                }
                 
-                resp.data.forEach(function(item) {
-                    map[item.id] = item;
+                pathSet.fields.forEach(function(field) {
+                    var path = [ pathSet[0], id, pathSet[2], field ];
+                    
+                    if(!map[id][field]) {
+                        return results.push({
+                            path  : path,
+                            value : {
+                                $type : "error",
+                                value : "Unknown field"
+                            }
+                        });
+                    }
+                    
+                    results.push({
+                        path  : path,
+                        value : map[id][field]
+                    });
                 });
-                
-                pathSet.ids.forEach(function(id) {
-                    pathSet[2].forEach(function(key) {
+            });
+            
+            return results;
+        });
+    }
+}, {
+    route : "itemsById[{integers:ids}]['buys', 'sells']['price', 'quantity']",
+    get   : function(pathSet) {
+        return axios.get(
+            "https://api.guildwars2.com/v2/commerce/prices?ids=" + pathSet.ids.join(",")
+        )
+        .then(function(resp) {
+            var map     = {},
+                results = [];
+            
+            resp.data.forEach(function(item) {
+                map[item.id] = item;
+            });
+            
+            pathSet.ids.forEach(function(id) {
+                pathSet[2].forEach(function(type) {
+                    pathSet[3].forEach(function(field) {
                         if(!map[id]) {
                             return results.push({
-                                path  : [ pathSet[0], id, key ],
+                                path  : [ pathSet[0], id, type, field ],
                                 value : {
                                     $type : "error",
                                     value : "Unknown item"
@@ -75,60 +136,15 @@ module.exports = require("falcor-router").createClass([ {
                             });
                         }
                         
-                        if(!map[id][key]) {
-                            return results.push({
-                                path  : [ pathSet[0], id, key ],
-                                value : {
-                                    $type : "error",
-                                    value : "Unknown field"
-                                }
-                            });
-                        }
-                        
                         results.push({
-                            path  : [ pathSet[0], id, key ],
-                            value : map[id][key]
+                            path  : [ pathSet[0], id, type, field ],
+                            value : map[id][type][field === "price" ? "unit_price" : field]
                         });
                     });
                 });
-                
-                return results;
             });
-    }
-}, {
-    route : "itemsById[{integers:ids}]['buys', 'sells']['price', 'quantity']",
-    get   : function(pathSet) {
-        return axios.get("https://api.guildwars2.com/v2/commerce/prices?ids=" + pathSet.ids.join(","))
-            .then(function(resp) {
-                var map     = {},
-                    results = [];
-                
-                resp.data.forEach(function(item) {
-                    map[item.id] = item;
-                });
-                
-                pathSet.ids.forEach(function(id) {
-                    pathSet[2].forEach(function(type) {
-                        pathSet[3].forEach(function(field) {
-                            if(!map[id]) {
-                                return results.push({
-                                    path  : [ pathSet[0], id, type, field ],
-                                    value : {
-                                        $type : "error",
-                                        value : "Unknown item"
-                                    }
-                                });
-                            }
-                            
-                            results.push({
-                                path  : [ pathSet[0], id, type, field ],
-                                value : map[id][type][field === "price" ? "unit_price" : field]
-                            });
-                        });
-                    });
-                });
-                
-                return results;
-            });
+            
+            return results;
+        });
     }
 } ]);
