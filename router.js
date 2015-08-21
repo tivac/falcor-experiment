bd4cfaa29d879741e536bc715b35499e6219fe7c
Pat Cavit <pcavit@gmail.com>
2015-08-20 23:14:08 -0700

    Recipes support in the router

--- router.js
+++ router.js
@@ -1,6 +1,7 @@
 "use strict";
 
-var axios = require("axios");
+var util  = require("util"),
+    axios = require("axios");
 
 module.exports = require("falcor-router").createClass([ {
     route : "items.length",
@@ -27,11 +28,10 @@ module.exports = require("falcor-router").createClass([ {
                     // Sometimes page starts won't line up nicely
                     diff  = range.from - start;
                 
-                console.log("https://api.guildwars2.com/v2/items?page=" + page + "&page_size=" + (size + diff));
-                
                 return axios.get(
-                    "https://api.guildwars2.com/v2/items?page=" + page + "&page_size=" + (size + diff)
-                ).then(function(resp) {
+                    util.format("https://api.guildwars2.com/v2/items?page=%d&page_size=%d", page, size + diff)
+                )
+                .then(function(resp) {
                     return resp.data
                         .sort(function(a, b) {
                             return a.id - b.id;
@@ -140,6 +140,109 @@ module.exports = require("falcor-router").createClass([ {
                             path  : [ pathSet[0], id, type, field ],
                             value : map[id][type][field === "price" ? "unit_price" : field]
                         });
+                    });
+                });
+            });
+            
+            return results;
+        });
+    }
+}, {
+    route : "recipes['input', 'output'][{integers:ids}].length",
+    get   : function(pathSet) {
+        return axios.all(
+            pathSet.ids.map(function(id) {
+                return axios.get(
+                    util.format("https://api.guildwars2.com/v2/recipes/search?%s=%d", pathSet[1], id)
+                )
+                .then(function(resp) {
+                    return {
+                        path  : [ pathSet[0], pathSet[1], id, pathSet[3] ],
+                        value : resp.data.length
+                    };
+                });
+            })
+        );
+    }
+}, {
+    route : "recipes['input', 'output'][{integers:ids}][{ranges:ranges}]",
+    get   : function(pathSet) {
+        return axios.all(
+            pathSet.ids.map(function(id) {
+                return axios.get(
+                    util.format("https://api.guildwars2.com/v2/recipes/search?%s=%d", pathSet[1], id)
+                )
+                .then(function(resp) {
+                    var paths = [];
+                    
+                    pathSet.ranges.forEach(function(range) {
+                        var x, value;
+                        
+                        for(x = range.from; x <= range.to; x++) {
+                            value = resp.data[x];
+                            
+                            paths.push({
+                                path  : [ pathSet[0], pathSet[1], id, x ],
+                                value : value ? {
+                                    $type : "ref",
+                                    value : [ "recipesById", value ]
+                                } : {
+                                    $type : "error",
+                                    value : "Invalid"
+                                }
+                            });
+                        }
+                    });
+                    
+                    return paths;
+                });
+            })
+        )
+        .then(function(results) {
+            return [].concat.apply([], results);
+        });
+    }
+}, {
+    route : "recipesById[{integers:ids}].details[{keys:fields}]",
+    get   : function(pathSet) {
+        return axios.get(
+            "https://api.guildwars2.com/v2/recipes?ids=" + pathSet.ids.join(",")
+        )
+        .then(function(resp) {
+            var map     = {},
+                results = [];
+            
+            resp.data.forEach(function(recipe) {
+                map[recipe.id] = recipe;
+            });
+            
+            pathSet.ids.forEach(function(id) {
+                if(!map[id]) {
+                    return results.push({
+                        path  : [ pathSet[0], id ],
+                        value : {
+                            $type : "error",
+                            value : "Unknown recipe"
+                        }
+                    });
+                }
+                
+                pathSet.fields.forEach(function(field) {
+                    var path = [ pathSet[0], id, pathSet[2], field ];
+                    
+                    if(!map[id][field]) {
+                        return results.push({
+                            path  : path,
+                            value : {
+                                $type : "error",
+                                value : "Unknown field"
+                            }
+                        });
+                    }
+                    
+                    results.push({
+                        path  : path,
+                        value : map[id][field]
                     });
                 });
             });
