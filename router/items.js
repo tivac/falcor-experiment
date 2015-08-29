b80d0a76fc847cc5e5ba1e960c20429674766762
Pat Cavit <pcavit@gmail.com>
2015-08-28 23:55:18 -0700

    Simplify to work like dyes

--- router/items.js
+++ router/items.js
@@ -1,7 +1,6 @@
 "use strict";
 
-var util  = require("util"),
-    axios = require("axios"),
+var axios = require("axios"),
     
     $ref  = require("falcor").Model.ref;
 
@@ -19,45 +18,20 @@ module.exports = [ {
 }, {
     route : "items[{ranges:ids}]",
     get   : function(pathSet) {
-        // Using axios all because we may have been passed multiple ranges
-        return axios.all(
-            pathSet.ids.map(function(range) {
-                // Adding one to size because ranges are inclusive
-                var size  = (range.to + 1) - range.from,
-                    page  = Math.floor(range.from / size),
-                    // Determine starting index so we can filter out results
-                    start = size * page,
-                    // Sometimes page starts won't line up nicely
-                    diff  = range.from - start;
+        return axios.get("https://api.guildwars2.com/v2/items")
+            .then(function(resp) {
+                var results = [];
                 
-                return axios.get(
-                    util.format("https://api.guildwars2.com/v2/items?page=%d&page_size=%d", page, size + diff)
-                )
-                .then(function(resp) {
-                    return resp.data
-                        .sort(function(a, b) {
-                            return a.id - b.id;
-                        })
-                        .filter(function(item, idx) {
-                            return start + idx >= range.from;
-                        })
-                        .map(function(item, idx) {
-                            return {
-                                path  : [ "items", range.from + idx ],
-                                value : $ref("itemsById." + item.id)
-                            };
+                pathSet.ids.forEach(function(range) {
+                    resp.data.slice(range.from, range.to + 1).forEach(function(id, idx) {
+                        results.push({
+                            path  : [ pathSet[0], range.from + idx ],
+                            value : $ref([ "itemsById", id ])
                         });
+                    });
                 });
-            })
-        )
-        .then(function(ranges) {
-            var paths = [];
-            
-            ranges.forEach(function(range) {
-                paths = paths.concat(range);
+                
+                return results;
             });
-            
-            return paths;
-        });
     }
 } ];
